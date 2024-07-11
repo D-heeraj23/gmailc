050a2a4e0c79cdc0e20d32ee9f249c07a2d5752a
Dhee0raj23 <dheeroy00@gmail.com>
2024-07-11 12:46:12 +0530

    sending and receiving mails are working

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BiStar } from "react-icons/bi";
+import { useState } from "react";
 
 const dummy = [
   {
@@ -23,19 +24,37 @@ const dummy = [
     message: "sending yhe gstr report online fill",
     time: "19:60",
   },
-  {
-    nameOfSender: "somekjsdk",
-    message:
-      "lokajsoij foijafjsd fjsdfjadjf lskajf ajflakjfioafjwejfosjflkasjfisaf js fjf slkjdf alsjf sjf lsjak flajksf ljsaf lajsf j ksjf aksjf lakjsf skljd fklsjdf lsakjf l;askjf laksjf lsakjfd lsdkjf lksdjf laskjf ;lsakjf las;kjf las;kfj lsakjf l;j",
-    time: "54:56",
-  },
 ];
 
 const Inbox = () => {
+  const [fetchedMails, setFetchedMails] = useState([]);
+  const email = localStorage.getItem("email");
+  const cleanedEmail = email.replace(/[@.]/g, "");
+  console.log(cleanedEmail);
+
+  useEffect(() => {
+    const fetchMails = async () => {
+      const response = await fetch(
+        `https://c-bc82f-default-rtdb.firebaseio.com/${cleanedEmail}.json`
+      );
+      const loadedMails = [];
+      const data = await response.json();
+      console.log(data, "data");
+      for (const keys in data) {
+        loadedMails.push({
+          subject: data[keys].subject,
+          textarea: data[keys].textarea,
+        });
+      }
+      setFetchedMails(loadedMails);
+    };
+    fetchMails();
+  }, []);
+  console.log(fetchedMails, "checking fetched mails");
   return (
     <div className="bg-[#f5f5f5] absolute top-20 left-[20rem] h-screen w-[85rem]">
       <div className="space-y-1">
-        {dummy.map((mails, i) => (
+        {fetchedMails.map((mails, i) => (
           <div
             key={i}
             className="bg-[#f3f3f3] flex justify-between items-center p-2"
@@ -47,10 +66,8 @@ const Inbox = () => {
                 </span>
               </button>
             </div>
-            <div className=" w-[15rem] text-lg font-bold">
-              {mails.nameOfSender}
-            </div>
-            <div className=" w-[60rem] font-bold">{mails.message}</div>
+            <div className=" w-[15rem] text-lg font-bold">{mails.subject}</div>
+            <div className=" w-[60rem] font-bold">{mails.textarea}</div>
             <div className=" w-[6rem] font-semibold">{mails.time}</div>
           </div>
         ))}
